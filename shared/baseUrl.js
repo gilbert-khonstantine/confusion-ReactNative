5c7e947685bbda976043a2c5bf76da65cc763280
gilbert-khonstantine <khonstantine@gmail.com>
2020-06-17 14:42:34 +0800

    set json server in heroku

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -5,4 +5,5 @@ import { Platform } from 'react-native';
 // export const baseUrl = 'http://10.0.2.2:3001/';
 let dummy = ''
 Platform.OS === 'ios' ? dummy = 'http://localhost:3001/' : dummy = 'http://10.0.2.2:3001/';
-export const baseUrl = dummy;
+// export const baseUrl = dummy;
+export const baseUrl = 'https://confusion-react-native.herokuapp.com/';
